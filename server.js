f2df3b1eb71db7e2ed38b5a54e70dfffc9f8c8b6
Fabricio Segura <130153388+StoneSoul@users.noreply.github.com>
2025-07-21 10:30:57 -0300

    Remove unused body-parser and mssql

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
 const usuariosRouter = require('./routes/usuarios');
