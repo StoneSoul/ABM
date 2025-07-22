84ccc3d4d7a165433adc4c27a969158b9e0cd2d6
Fabricio Segura <130153388+StoneSoul@users.noreply.github.com>
2025-07-22 16:52:44 -0300

    fix auth header for wordpress api

--- services/wpSyncService.js
+++ services/wpSyncService.js
@@ -2,6 +2,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const axios = require('axios');
 const WP_API = process.env.WP_API;
+const WP_TOKEN = process.env.WP_TOKEN;
 
 
 exports.syncToWordpress = async ({
@@ -24,6 +25,10 @@ exports.syncToWordpress = async ({
       nombre,
       apellido,
       alias: alias || username // si no viene alias, usa username
+    }, {
+      headers: {
+        Authorization: `Bearer ${WP_TOKEN}`
+      }
     });
   } catch (error) {
     console.error('Error sincronizando con WordPress:', error.response?.data || error);
@@ -36,6 +41,10 @@ exports.actualizarEstado = async ({ username, estado }) => {
     await axios.post(`${WP_API}/update-user-status`, {
       username,
       enabled: estado === 1,
+    }, {
+      headers: {
+        Authorization: `Bearer ${WP_TOKEN}`
+      }
     });
   } catch (error) {
     console.error('Error actualizando estado en WordPress:', error.response?.data || error);
