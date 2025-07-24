ab4852222d81ef791892e2768b55796ec8ee8b1c
StoneSoul <fabricio.segura@hotmail.com>
2025-07-24 11:24:15 -0300

    intento de actualizar el front

--- public/header.js
+++ public/header.js
@@ -7,6 +7,16 @@ async function loadLayout() {
     const resFooter = await fetch('footer.html');
     const htmlFooter = await resFooter.text();
     document.getElementById('footer-placeholder').innerHTML = htmlFooter;
+
+    // Agregar evento logout luego de insertar el header
+    const logoutBtn = document.getElementById('btnLogout');
+    if (logoutBtn) {
+      logoutBtn.addEventListener('click', async () => {
+        await fetch('/api/auth/logout', { method: 'POST' });
+        window.location.href = '/login.html';
+      });
+    }
+
   } catch (err) {
     console.error('Error cargando cabecera o pie:', err);
   }
