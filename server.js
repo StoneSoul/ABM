a25e5459c4441392dd63929d80c38daf286cacf4
Fabricio Segura <fabricio.segura@hotmail.com>
2025-09-26 16:11:07 -0300

    cambio en .env

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const sessionStore = new MySQLStore({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
